2a83b30a408b9e7d9f8de1fe1ac8c017c2edd91f
Lucas de Assis Dias <lucas1000lad@gmail.com>
2017-12-07 23:58:22 -0200

    Adicionado aula 2 e arquivo de leitura

---
+++ aula2.js
@@ -0,0 +1,19 @@
+var http = require('http')
+var arquivo = require('fs') // file system
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-type': 'text/html' })
+  // __dirname retorna o diretório do projeto
+  arquivo.readFile(`${__dirname}/arquivos/index.html`, function (err, html) {
+    if (err) {
+      res.write('Arquivo index.html não encontrado')
+    } else {
+      res.write(html)
+    }
+    res.end()
+  })
+})
+
+server.listen(3000, function () {
+  console.log('Servidor rodando na porta 3000')
+})
